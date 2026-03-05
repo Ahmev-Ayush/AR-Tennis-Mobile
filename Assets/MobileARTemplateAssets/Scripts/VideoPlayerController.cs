06e0b7df6ff36476bb8290b1234c5b3ef0590aee
Ayush <ayushomr1103@gmail.com>
2026-03-05 19:29:07 +0530

    fix(videoPlayerIssue): cube replace with quad to play video on one side

---
+++ Assets/MobileARTemplateAssets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Video; // VideoPlayer namespace for video playback
+
+public class VideoPlayerController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer; // Reference to the VideoPlayer component
+
+    void Start()
+    {
+        // 1. Get the VideoPlayer component attached to the same GameObject
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.prepareCompleted += OnVideoPrepared; // Subscribe to the prepareCompleted event
+        videoPlayer.Prepare(); // Prepare the video for playback
+
+        // 2. set the video in loop mode
+        videoPlayer.isLooping = true;
+
+        // 3. Start playing the video
+        videoPlayer.Play();
+    }
+
+    public void PlayMyVideo()
+    {
+        if(videoPlayer != null)
+        {
+            videoPlayer.Play(); // Play the video
+            Debug.Log("Video is playing.");
+        }
+    }
+
+    public void PauseMyVideo()
+    {
+        videoPlayer.Pause(); // Pause the video
+        Debug.Log("Video is paused.");
+    }
+
+    void OnVideoPrepared(VideoPlayer vp)
+    {
+        Debug.Log("Video is prepared and ready to play.");
+        videoPlayer.Play(); // Automatically play the video once it's prepared
+    }
+
+}
