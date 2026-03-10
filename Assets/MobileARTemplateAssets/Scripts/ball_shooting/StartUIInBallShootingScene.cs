1bd23cbc261c02e5c9621f7d0940e3e20ecf7564
Ayush <ayushomr1103@gmail.com>
2026-03-11 01:59:16 +0530

    feat(BallShootingExtension) : a basic tutorial guide for court placement

--- Assets/MobileARTemplateAssets/Scripts/ball_shooting/StartUIInBallShootingScene.cs
+++ Assets/MobileARTemplateAssets/Scripts/ball_shooting/StartUIInBallShootingScene.cs
@@ -1,22 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 
 public class StartUIInBallShootingScene : MonoBehaviour
 {
     [Header("UI Panels")]
     public GameObject welcomePanel;
     public GameObject adjustPanel;
+    public GameObject TutorialPanel;
+    public GameObject TutorialPanel2;
+    public Button buttonToContinue;
+
 
     void Start()
     {
         // When the app starts, show Welcome and hide Adjust
         welcomePanel.SetActive(true);
         adjustPanel.SetActive(false);
+        TutorialPanel.SetActive(false);
+        TutorialPanel2.SetActive(false);
+        buttonToContinue.gameObject.SetActive(false);
     }
 
     // Called by the 'Start' button on the Welcome Panel
     public void StartSetup()
     {
         welcomePanel.SetActive(false);
+        // adjustPanel.SetActive(true);
+        TutorialPanel.SetActive(true);
+        TutorialPanel2.SetActive(true);
+        buttonToContinue.gameObject.SetActive(true);
+    }
+
+    public void ShowAdjustmentPanel()
+    {
+        TutorialPanel.SetActive(false);
+        TutorialPanel2.SetActive(false);
         adjustPanel.SetActive(true);
     }
 
@@ -24,6 +43,7 @@ public class StartUIInBallShootingScene : MonoBehaviour
     public void FinishAndLock()
     {
         adjustPanel.SetActive(false);
+
         // The UI is now gone, and the user can play!
     }
 }
